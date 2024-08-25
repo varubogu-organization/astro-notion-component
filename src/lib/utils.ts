793b37e4da22170b9d797fe4086fcab1bb813bf0
varubogu <toyosuke@varubogu.com>
2024-08-25 04:42:44 +0000

    NotionURLの正規表現を修正（−区切りが必須になっていた）

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,7 +17,7 @@ export const notionUrlToPageId = (url: string | URL) => {
   if (url instanceof URL) {
     url = url.toString()
   }
-  const match = url.match(/https:\/\/(www\.)?notion\.so\/.*-([0-9a-f]{32})/);
+  const match = url.match(/https:\/\/(www\.)?notion\.so\/[^/]*([0-9a-f]{32})/);
   if (!match) {
     throw new Error('Invalid Notion URL');
   }
